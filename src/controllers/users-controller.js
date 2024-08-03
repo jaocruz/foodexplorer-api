369e5099d54dca30c5446d6b062f223a903e9ac7
João <mcruznq@gmail.com>
2024-08-03 20:41:39 -0300

    Revert "update user"
    This reverts commit e109ff58b5a6d4a0b1c36fd1d81c3850c93cc9cc.

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -19,32 +19,7 @@ class UsersController {
     await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
 
     return response.status(201).json();    
-  };
-
-  async update (request, response) {
-    const { name, email } = request.body;
-    const { id } = request.params;
-
-    const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
-
-    if(!user){
-      throw new appError("Usuário não encontrado.")
-    };
-
-    const updatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
-
-    if(updatedEmail && updatedEmail.id !== user.id){
-      throw new appError("Esse e-mail já está em uso.")
-    };
-
-    user.name = name;
-    user.email = email;
-
-    await database.run("UPDATE users SET name = ?, email = ? WHERE id = ?", [user.name, user.email, id]);
-
-    return response.status(200).json();
-  };
+  }
 }
 
 module.exports = UsersController;
