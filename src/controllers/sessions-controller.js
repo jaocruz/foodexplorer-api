729b103b55ef2cb27fe3cf0b710890263b9732e7
João <mcruznq@gmail.com>
2024-08-19 19:55:22 -0300

    authenticated middleware

--- src/controllers/sessions-controller.js
+++ src/controllers/sessions-controller.js
@@ -1,7 +1,8 @@
-const knex = require("../database/knex");
-const appError = require("../utils/app-error");
-
 const { compare } = require("bcryptjs");
+const { sign } = require("jsonwebtoken");
+const knex = require("../database/knex");
+const authConfig = require("../configs/auth");
+const appError = require("../utils/app-error");
 
 class SessionsController {
   async create(request, response){
@@ -19,7 +20,14 @@ class SessionsController {
       throw new appError("E-mail e/ou senha incorreta", 401);
     }
 
-    return response.json({user})
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    });
+
+    return response.json({ user, token })
   }
 }
 
