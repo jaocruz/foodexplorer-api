abd7c82716ab0f73445a255ea33816d07fd71ef0
João <mcruznq@gmail.com>
2024-08-21 20:56:42 -0300

    saving image in db

---
+++ src/controllers/dish-photo-controller.js
@@ -0,0 +1,27 @@
+const knex = require("../database/knex");
+const appError = require("../utils/app-error");
+const DiskStorage = require("../providers/disk-storage");
+
+class DishPhotoController {
+  async update(request, response) {
+    const dish_id = request.params.id;
+    const photoFilename = request.file.filename;
+
+    const diskStorage = new DiskStorage();
+
+    const dish = await knex("dishs").where({ id: dish_id }).first();
+
+    if(dish.photo) {
+      await diskStorage.deleteFile(dish.photo);
+    }
+
+    const filename = await diskStorage.saveFile(photoFilename);
+    dish.photo = filename;
+
+    await knex("dishs").update(dish).where({ id: dish_id });
+
+    return response.json(dish);
+  }
+}
+
+module.exports = DishPhotoController;
--- src/routes/dishs-routes.js
+++ src/routes/dishs-routes.js
@@ -4,11 +4,13 @@ const multer = require("multer");
 const uploadConfig = require("../configs/upload");
 
 const DishsController = require("../controllers/dishs-controller");
+const DishsPhotoController = require("../controllers/dish-photo-controller");
 
 const dishsRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const dishsController = new DishsController();
+const dishPhotoController = new DishsPhotoController();
 
 dishsRoutes.get("/", dishsController.index);
 dishsRoutes.get("/:id", dishsController.show);
@@ -16,9 +18,6 @@ dishsRoutes.post("/", dishsController.create);
 dishsRoutes.put("/:id", dishsController.update);
 dishsRoutes.delete("/:id", dishsController.delete);
 
-dishsRoutes.patch("/:id/photo", upload.single("photo"), (request, response) => {
-  console.log(request.file.filename);
-  response.json();
-})
+dishsRoutes.patch("/:id/photo", upload.single("photo"), dishPhotoController.update);
 
 module.exports = dishsRoutes;
